20caa56a8d10997db43a721f252348261efdd6bd
Ircama <Ircama@users.noreply.github.com>
2025-06-15 18:14:58 +0200

    Version 6.2.7

--- ui.py
+++ ui.py
@@ -37,7 +37,7 @@ from find_printers import PrinterScanner
 from text_console import TextConsole
 
 
-VERSION = "6.2.5"
+VERSION = "6.2.7"
 
 NO_CONF_ERROR = (
     " Please select a printer model and a valid IP address,"
