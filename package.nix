3d2deeb39b28fd6cb73dd1131f168bc494d8b5ce
Ircama <Ircama@users.noreply.github.com>
2026-09-11 14:23:33 +0200

    Bump version to 7.3.3

--- package.nix
+++ package.nix
@@ -7,7 +7,7 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "epson_print_conf";
-  version = "7.3.2";
+  version = "7.3.3";
 
   src = fetchFromGitHub {
     owner = "Ircama";
--- ui.py
+++ ui.py
@@ -42,7 +42,7 @@ from text_console import TextConsole
 from epson_escp2.epson_encode import TextToImageConverter, EpsonEscp2
 
 
-VERSION = "7.3.2"
+VERSION = "7.3.3"
 
 NO_CONF_ERROR = (
     " Please select a printer model and a valid IP address,"
